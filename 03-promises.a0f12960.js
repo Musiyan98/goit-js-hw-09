3dd613728cf38e78744c5629b34c71db24cbaf5e
Musiyan98 <98363265+Musiyan98@users.noreply.github.com>
2022-09-28 20:39:27 +0000

    Deploying to gh-pages from @ Musiyan98/goit-js-hw-09@5f0438991ef60895a6a91bc384a7e131b77b577b 🚀

---
+++ 03-promises.a0f12960.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".form");document.querySelector('input[name="delay"]'),document.querySelector('input[name="step"]'),document.querySelector('input[name="amount"]');e.addEventListener("submit",(function(e,t){Math.random()}));
+//# sourceMappingURL=03-promises.a0f12960.js.map
